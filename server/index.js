92b3d3a69730c5937bb67ef7f606ea7bca43de88
Keith Radin <keith@Keiths-MacBook-Pro.local>
2020-04-23 14:01:10 -0400

    Added client

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ app.use(cors());
 const users = require("./routes/api/users");
 app.use("/api/users", users);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(`${__dirname}/public/`));
+  app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Node listening on port ${port}`);
