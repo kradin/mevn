c8e41dd35aa8a6deaae7ce109e2c160998b6257a
Keith Radin <keith@Keiths-MacBook-Pro.local>
2020-04-22 20:25:16 -0400

    Setup server

--- server/index.js
+++ server/index.js
@@ -12,5 +12,5 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Node listening on port ${port}`);
+  console.log(`Node listening on port ${port}`);
 });
